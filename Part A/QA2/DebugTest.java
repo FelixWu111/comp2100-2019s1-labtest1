56245e194dcda2e6cdb23d2e7656e14b07086c4b
Sid Chau <sid.chau@anu.edu.au>
2019-03-26 16:08:21 +1100

    Add new file

---
+++ Part A/QA2/DebugTest.java
@@ -0,0 +1,7 @@
+public class DebugTest {
+  public static void main(String[] args) { method1(); }
+  private static void method1() {
+      System.out.println("Statement 1 of method 1");
+      System.out.println("Statement 2 of method 1");
+      System.out.println("Statement 3 of method 1");}
+}
