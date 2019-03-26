c322562a7d839f05d22d224c1c98e7718ed91942
Dongwoo Kim <dongwoo.kim@anu.edu.au>
2019-03-26 16:06:03 +1100

    minor update

--- Part A/QA4/Collections.java
+++ Part A/QA4/Collections.java
@@ -7,24 +7,4 @@ public class Collections {
 	public static void copy(List output, List input) {
 	}
 
-	// Test code, do not modify
-	public static void main(String[] args) {
-
-		List<Number> output = new ArrayList<Number>();
-		List<Integer> input = new ArrayList<Integer>();
-		input.add(5);
-		Collections.copy(output, input);
-		System.out.println(output.get(0));
-
-		List<Integer> output3 = new ArrayList<Integer>();
-		List<Integer> input3 = new ArrayList<Integer>();
-		input3.add(3);
-		Collections.copy(output3, input3);
-		System.out.println(output.get(0));
-
-		List<String> output2 = new ArrayList<String>();
-		List<Long> input2 = new ArrayList<Long>();
-
-		// Collections.copy(output2, input2); // will raise error
-	}
 }
