4e47bd04f521f8b4c007147c284d652c2f5dc5bf
Duy Khuu <u6380923@anu.edu.au>
2019-03-28 18:13:08 +1100

    Fix test case for QB3

--- Part B/QB3/QB3Test.java
+++ Part B/QB3/QB3Test.java
@@ -19,7 +19,7 @@ public class QB3Test {
 		System.out.println("Test 2: " + (((tree1.leastCommonAncestor(8 , 9) != null) && (tree1.leastCommonAncestor(8 , 9) == 7)) ? "PASS" : "FAIL"));
 		System.out.println("Test 3: " + (((tree2.leastCommonAncestor(1 , 6) != null) && (tree2.leastCommonAncestor(1 , 6) == 5)) ? "PASS" : "FAIL"));
 		System.out.println("Test 4: " + (((tree2.leastCommonAncestor(10 , 6) != null) && (tree2.leastCommonAncestor(10 , 6) == 7)) ? "PASS" : "FAIL"));
-		System.out.println("Test 5: " + (((tree3.leastCommonAncestor(1 , 37) != null) && (tree3.leastCommonAncestor(1 , 37) == 23)) ? "PASS" : "FAIL"));
+		System.out.println("Test 5: " + (((tree3.leastCommonAncestor(1 , 37) != null) && (tree3.leastCommonAncestor(1 , 37) == 30)) ? "PASS" : "FAIL"));
 		System.out.println("Test 6: " + (((tree3.leastCommonAncestor(6 , 51) != null) && (tree3.leastCommonAncestor(6 , 51) == 30)) ? "PASS" : "FAIL"));
 
 	}
